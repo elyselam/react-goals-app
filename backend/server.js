5e9c5acd83231abf9e3919117e170b2c6991da95
elyselam <elyse.p.lam@gmail.com>
2022-03-09 18:38:33 -0800

    create controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("hi")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+const app = express()
+
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({message:'Get goals'})
+})
+app.listen(port, () => console.log(`server started on port ${port}`))
